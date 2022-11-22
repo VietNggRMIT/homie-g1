87356af8649ee59d9a803d492c553c37c22f5b5a
vietanhrmit <nva6880@gmail.com>
2022-11-22 22:53:01 +0700

    email -> phone

--- php1/account.php
+++ php1/account.php
@@ -6,7 +6,7 @@
     else{ //landlord logged in, lets print info
         echo "Welcome landlord " . $_SESSION['user']['fullname'] . ", username " . $_SESSION['user']['uname'] . "<br>";
         echo "<img src=\"" . $_SESSION['user']['pfp'] . "\" height=\"150\"></img>";
-        echo "<br>Your email is " . $_SESSION['user']['email'] . " and your phone number is " . $_SESSION['user']['email'];
+        echo "<br>Your email is " . $_SESSION['user']['email'] . " and your phone number is " . $_SESSION['user']['phone'];
         echo "<br>Below are your listings:<br>";
     }
 ?>
