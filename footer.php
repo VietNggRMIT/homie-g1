dfecc92757c947ed9b8aed873b1a8d9795a2fbc4
Viet Nguyen <viet.ngg21@gmail.com>
2022-11-28 16:21:12 +0700

    html scss for signup block

---
+++ footer.php
@@ -0,0 +1,61 @@
+<footer class="footer">
+    <div class="footer-wrapper">
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-sm-12 col-md-6 about-section">
+                    <h6>A few word</h6>
+                    <p class="text-justify">
+                    Our group HOMIE (House Ordering & Management Intelligence Enterprise) has decided to solve house rental 
+                    and room rental problems by creating a web app solution that works as a central ecommerce platform for 
+                    tenant and landlord to connect, advertise or buy service. We aimed to solve the transparency of renting 
+                    contracts, as well as pricing and provide a managing solution for landlords with multiple housing for 
+                    rent. This is our demo product but will not be our final product as we can expand it bigger in 
+                    the future should we pursue it.
+                    </p>
+                </div>
+        
+                <div class="col-xs-6 col-md-3">
+                    <h6>Categories</h6>
+                    <ul class="footer-links">
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                    </ul>
+                </div>
+        
+                <div class="col-xs-6 col-md-3">
+                    <h6>Other</h6>
+                    <ul class="footer-links">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+
+            </div>
+            <hr>
+        </div>
+
+        <div class="container">
+            <div class="row">
+            <div class="col-md-7 col-sm-6 col-xs-12">
+                <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by 
+                <a href="#">Homie (Group 1 Tutorial Hanoi)</a>.
+                </p>
+            </div>
+    
+            <div class="col-md-5 col-sm-6 col-xs-12">
+                <ul class="social-icons">
+                <img class="footer-logo" src="\php1\resources\logo\Homie-rental.png" alt="logo">
+                <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a class="github" href="https://github.com/VietNggRMIT/homie-g1"><i class="fa fa-github"></i></a></li>  
+                </ul>
+            </div>
+            </div>
+        </div>
+    </div>
+</footer>
