6c9a51a01fe9277efb855799d80c439466317d0f
Viet Nguyen <viet.ngg21@gmail.com>
2022-12-13 12:09:51 +0700

    jquery code for dynamic number of carousel

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(".slide").each(function(i) {
+    var carouselCard = $(this)
+    //append number in ascending order to id of carousel
+    carouselCard.attr('id', 'carouselControls' + i);
+    var controlValue = $(this).attr('id');
+    //select carousel control buttons
+    var carouselBtn = carouselCard.children("button");
+    //change button data accordingly
+    carouselBtn.attr('data-bs-target', '#carouselControls' + i)
+    // console.log(controlValue);
+    // console.log(carouselBtn.data('bs-target'));
+});
