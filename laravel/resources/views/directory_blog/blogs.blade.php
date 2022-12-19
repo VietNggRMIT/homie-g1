91e14b7a735293832e25731dde622c866b6364d3
Giang Trong Duong <92968238+GiangTrongDuong@users.noreply.github.com>
2022-12-19 15:14:28 +0700

    Re-work on blog-listing site

--- laravel/resources/views/directory_blog/blogs.blade.php
+++ laravel/resources/views/directory_blog/blogs.blade.php
@@ -63,45 +63,36 @@
                         </div>
                     </div>
                 </div>
-        <div class="breadcrumb justify-content-center">
-            <h2>{{ Breadcrumbs::render('breadcrumb_blogs') }}</h2>
-        </div>
-        @foreach($blogs as $blog)
-        <div class="blog-container">
-                    <div class="card border" style="width: 100%;">
-                        <div class="card-body">
+        <div class="blogs-wrapper">
+            <div class="breadcrumb justify-content-center">
+                <h2>{{ Breadcrumbs::render('breadcrumb_blogs') }}</h2>
+            </div>
+            @foreach($blogs as $blog)
+            <div class="blog-container align-item-center">
+                        <div class="card border" style="width: 100%;">
+                            <div class="card-body">
                             <div class="row">
-                                <div class="col-lg-4">
+                                <div class="col-md-8">
                                     <h5 class="card-title"><b><button onclick="window.location.href='{{ route('blogs.show', ['blog' => $blog]) }}';"
-                                            type="button"
-                                        >
-                                            {{ $blog->blog_name }}
-                                        </button></b></h5>
-                                    <hr>
+                                                type="button"><b>{{ $blog->blog_name }}</button></b></b></h5>
+                                    <p><i class="fa-solid fa-calendar-days"></i> {{ $blog->created_at }}  <i class="fa-solid fa-hashtag"></i>{{ $blog->id }}</p>
+                                </div>
+                                <div class="col-md-4">
                                     <div class="blog-info">
-                                        <p><b>Blog ID:</b>{{ $blog->id }}</h6></p>
-                                        <p><b>Released at:</b>{{ $blog->created_at }}</p>
-                                        <p><b>Updated at:</b> {{ $blog->updated_at }}</p>
-                                        <br>
-                                        <p><b>Posted by:</b><button class="user-name"
-                                            onclick="window.location.href='{{ route('users.show', ['user' => $blog->user]) }}';"
-                                            type="button"
-                                        >
-                                            {{ $blog->user->user_real_name }}
-                                        </button><br></p>
-                                        <p><b>Id:</b>{{ $blog->user->id }}</p>
+                                            <p><i class="fa-regular fa-id-card"></i><button class="user-name"
+                                                onclick="window.location.href='{{ route('users.show', ['user' => $blog->user]) }}';"
+                                                type="button"
+                                            >
+                                                {{ $blog->user->user_real_name }}
+                                            </button> <i class="fa-solid fa-hashtag"></i>{{ $blog->user->id }}</p>
+                                            <p><b>Updated at:</b> {{ $blog->updated_at }}</p>
                                     </div>
                                 </div>
-                                <div class="col-lg-8">
-                                    <h5><b>Decription</b></h5>
-                                    <hr>
-                                    <p class="card-text">{!!  nl2br(e($blog->blog_description)) !!}</p>
-                                </div>
+</div>
                             </div>
                         </div>
-                      </div>
-                </div>
-            <div>
+                    </div>
+                <div>
                 <!-- <h2>Blog</h2>
                 <div>$blog->id: {{ $blog->id }}</div>
                 <div>
