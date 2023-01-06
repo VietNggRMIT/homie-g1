74c8ebefcbaa9151b78744cd00e926b4b744a5be
vantocanhsang <pancakes21f@gmail.com>
2023-01-06 21:03:29 +0700

    Added title to two pages

--- laravel/resources/views/directory_user/user_change_password.blade.php
+++ laravel/resources/views/directory_user/user_change_password.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
-
+@section('title', 'Change Password')
 @section('content')
+
 <div class="container">
 
     <div class="row account-bg my-5">
--- laravel/resources/views/directory_user/user_update.blade.php
+++ laravel/resources/views/directory_user/user_update.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', 'Update Information')
 @section('content')
 <div class="container">
 
